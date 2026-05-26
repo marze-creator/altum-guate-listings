a9172c6d634a7827f1ec3ca1b92ac11e2b50225c
marze-creator <marzellorojas@gmail.com>
2026-05-26 13:38:03 -0600

    Update _vendedor.vendedores.propiedades.nueva.tsx

--- src/routes/_vendedor.vendedores.propiedades.nueva.tsx
+++ src/routes/_vendedor.vendedores.propiedades.nueva.tsx
@@ -1,4 +1,4 @@
-mport { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { supabase } from "@/integrations/supabase/client";
 import { useAuth } from "@/hooks/use-auth";
